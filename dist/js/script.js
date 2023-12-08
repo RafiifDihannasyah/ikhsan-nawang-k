ba803f4fd4ce6274efedbfe445e5b81e57c144fa
Rafiif Dihannasyah <rafiifdihannasyah1@gmail.com>
2023-12-08 16:32:45 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,31 @@
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixednav = header.offsetTop;
+    const toTop = document.querySelector('#to-top')
+    if (window.pageYOffset > fixednav) {
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    } else {
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+    
+}
+
+
+const humbuger = document.querySelector('#hambuger');
+const navmenu = document.querySelector('#nav-menu')
+
+hambuger.addEventListener('click', function() {
+    hambuger.classList.toggle('hambuger-active');
+    navmenu.classList.toggle('hidden');
+});
+
+window.addEventListener('click' , function (e) {
+    if(e.target != hambuger && e.target != navmenu) {
+        hambuger.classList.remove('hambuger-active');
+        navmenu.classList.add('hidden');
+    }
+}) 
